dd01d64f2b373d4dc90847c48142589f39060252
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-28 11:10:44 -0300

    FaunaDB integration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../lib/fauna'
+import { query as q } from 'faunadb'
 
 export const authOptions = {
     providers: [
@@ -14,5 +16,37 @@ export const authOptions = {
             }
         }),
     ],
+
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'), q.Casefold(user.email)
+                                ),
+                            ),
+                        ),
+                        q.Create(
+                            q.Collection('users'), { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'), q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true
+
+            } catch {
+                return false;
+            }
+        }
+    }
 }
 export default NextAuth(authOptions)
