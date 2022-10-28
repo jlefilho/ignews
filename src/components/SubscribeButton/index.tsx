1231e9ce27aae041f7d1b4cc1039d4400f9a8c4e
Jorge Espindola <jlespindolaf@gmail.com>
2022-10-28 14:43:39 -0300

    Stripe checkout redirection

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../lib/api';
+import { getStripeJs } from '../../lib/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -8,12 +10,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }:SubscribeButtonProps) {
     const {data: session} = useSession()
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return
         }
 
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+
+        } catch (err) {
+            alert(err.message)
+        }
     }
 
     return (
